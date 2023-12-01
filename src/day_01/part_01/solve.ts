1b1dc9b56ced0ca26430ad8968f11511fb6ce978
John Riccardi <riccjohn@gmail.com>
2023-12-01 14:15:57 -0600

    Start Day 01 pt 2

--- src/day_01/part_01/solve.ts
+++ src/day_01/part_01/solve.ts
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs'
 import { getCalibrationValuesAndSum } from "./day_01";
 
 const solve = (): number => {
-  const input = readFileSync('input.txt', 'utf-8')
+  const input = readFileSync('../input.txt', 'utf-8')
 
   const lines = input.split('\n')
 
