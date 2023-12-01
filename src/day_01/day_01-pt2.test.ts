af28e8bcd3236d76725d797807184b73cd78ebfd
John Riccardi <riccjohn@gmail.com>
2023-12-01 17:53:32 -0600

    WIP: This is incorrect for day 01 pt 2

---
+++ src/day_01/day_01-pt2.test.ts
@@ -0,0 +1,54 @@
+import { getCalibrationValue, getCalibrationValuesAndSum } from './day_01'
+
+const testDataWithCalibrationValues = [
+  /* spell-checker: disable */
+  { input: 'two1nine', expected: 29 },
+  { input: 'eightwothree', expected: 83 },
+  { input: 'abcone2threexyz', expected: 13 },
+  { input: 'xtwone3four', expected: 24 },
+  { input: '4nineeightseven2', expected: 42 },
+  { input: 'zoneight234', expected: 14 },
+  { input: '7pqrstsixteen', expected: 76 },
+  /* spell-checker: enable */
+]
+
+describe('getCalibrationValue', () => {
+  // spell-checker: disable
+  test.each`
+    input                                     | expected
+    ${testDataWithCalibrationValues[0].input} | ${testDataWithCalibrationValues[0].expected}
+    ${testDataWithCalibrationValues[1].input} | ${testDataWithCalibrationValues[1].expected}
+    ${testDataWithCalibrationValues[2].input} | ${testDataWithCalibrationValues[2].expected}
+    ${testDataWithCalibrationValues[3].input} | ${testDataWithCalibrationValues[3].expected}
+    ${testDataWithCalibrationValues[4].input} | ${testDataWithCalibrationValues[4].expected}
+    ${testDataWithCalibrationValues[5].input} | ${testDataWithCalibrationValues[5].expected}
+    ${testDataWithCalibrationValues[6].input} | ${testDataWithCalibrationValues[6].expected}
+  `(
+    // spell-checker: enable
+    'given an input of $input, returns $expected',
+    ({ input, expected }) => {
+      const result = getCalibrationValue(input)
+
+      expect(result).toBe(expected)
+    },
+  )
+})
+
+
+describe('getCalibrationValuesAndSum', () => {
+  test('given an array of calibration values including spelled out numbers, returns the sum of the calibration values', () => {
+    const input = [
+      testDataWithCalibrationValues[0].input,
+      testDataWithCalibrationValues[1].input,
+      testDataWithCalibrationValues[2].input,
+      testDataWithCalibrationValues[3].input,
+      testDataWithCalibrationValues[4].input,
+      testDataWithCalibrationValues[5].input,
+      testDataWithCalibrationValues[6].input,
+    ]
+
+    const result = getCalibrationValuesAndSum(input)
+
+    expect(result).toBe(281)
+  })
+})
--- src/day_01/day_01.ts
+++ src/day_01/day_01.ts
@@ -1,6 +1,27 @@
 export const getCalibrationValue = (inputValue: string): number => {
+  const numberNameDictionary: { [key: string]: string } = {
+    one: '1',
+    two: '2',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6',
+    seven: '7',
+    eight: '8',
+    nine: '9',
+  }
+
+  const numberNameRegex = new RegExp(Object.keys(numberNameDictionary).join('|'), 'gm')
+  const numberNameMatches = inputValue.match(numberNameRegex)
+
+  if (numberNameMatches) {
+    numberNameMatches.forEach(match => {
+      inputValue = inputValue.replace(match, numberNameDictionary[match])
+    })
+  }
+
   const numbers = inputValue.trim().match(/\d/gm)
-  
+
   if (!numbers) {
     return 0
   }
