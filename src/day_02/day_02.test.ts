2c8b08f9ad913084d3a9db2d9f9352df9a12d840
John Riccardi <riccjohn@gmail.com>
2023-12-02 12:45:43 -0600

    Day02 Add parser

--- src/day_02/day_02.test.ts
+++ src/day_02/day_02.test.ts
@@ -1,18 +1,75 @@
-import { parseGame } from "./day_02"
+import { parseGame } from './day_02'
 
 describe('parseGame()', () => {
-  test('returns array of game sets', () => {
-    const input = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'
-
-    const expectedResult = {
+  const testInputWithAnswers = [
+    {
+      input: 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+      expected: {
         game: 1,
         sets: [
-            { blue: 3, green: 0, red: 4 },
-            { blue: 6, green: 2, red: 1 },
-            { blue: 0, green: 2, red: 0 },
-        ]
-    }
+          { blue: 3, red: 4 },
+          { blue: 6, green: 2, red: 1 },
+          { green: 2 },
+        ],
+      },
+    },
+    {
+      input: 'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
+      expected: {
+        game: 2,
+        sets: [
+          { blue: 1, green: 2 },
+          { green: 3, blue: 4, red: 1 },
+          { green: 1, blue: 1 },
+        ],
+      },
+    },
+    {
+      input:
+        'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+      expected: {
+        game: 3,
+        sets: [
+          { green: 8, blue: 6, red: 20 },
+          { blue: 5, red: 4, green: 13 },
+          { green: 5, red: 1 },
+        ],
+      },
+    },
+    {
+      input:
+        'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
+      expected: {
+        game: 4,
+        sets: [
+          { green: 1, red: 3, blue: 6 },
+          { green: 3, red: 6 },
+          { green: 3, blue: 15, red: 14 },
+        ],
+      },
+    },
+    {
+      input: 'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green',
+      expected: {
+        game: 5,
+        sets: [
+          { red: 6, blue: 1, green: 3 },
+          { blue: 2, red: 1, green: 2 },
+        ],
+      },
+    },
+  ]
 
-    expect(parseGame(input)).toBe(expectedResult)
+  test.each`
+    input                            | expected
+    ${testInputWithAnswers[0].input} | ${testInputWithAnswers[0].expected}
+    ${testInputWithAnswers[1].input} | ${testInputWithAnswers[1].expected}
+    ${testInputWithAnswers[2].input} | ${testInputWithAnswers[2].expected}
+    ${testInputWithAnswers[3].input} | ${testInputWithAnswers[3].expected}
+    ${testInputWithAnswers[4].input} | ${testInputWithAnswers[4].expected}
+  `('given an input of $input, returns $expected', ({ input, expected }) => {
+    const result = parseGame(input)
+
+    expect(result).toStrictEqual(expected)
   })
 })
--- src/day_02/day_02.ts
+++ src/day_02/day_02.ts
@@ -1,30 +1,35 @@
-
 type Color = 'blue' | 'green' | 'red'
 
-interface Set {
-    
+type Set = {
+  [key in Color]: number
 }
-
 
 interface Game {
   game: number
-  sets: [
-
-  ]
-  results: {
-    blue: number
-    green: number
-    red: number
-  }
+  sets: Set[]
 }
 
 export const parseGame = (input: string): Game => {
+  const [rawGame, rawSets] = input.split(':')
+
+  const game = Number(rawGame.split(' ')[1])
+
+  const sets = rawSets
+    .split(';')
+    .map(set => set.trim())
+    .map(set => set.split(', '))
+    .map(set =>
+      set.reduce((acc, curr) => {
+        const [occurrances, color] = curr.split(' ') as [string, Color]
+
+        acc[color] = Number(occurrances)
+
+        return acc
+      }, {} as Set),
+    )
+
   return {
-    game: 1,
-    results: {
-      blue: 0,
-      green: 0,
-      red: 0,
-    },
+    game,
+    sets,
   }
 }
