db1b3ef0a14afc9b1ac27316f3fc4a4d789551be
John Riccardi <riccjohn@gmail.com>
2023-12-02 14:05:37 -0600

    Day02 Add ability to sum up all possible game ids

--- src/day_02/day_02.test.ts
+++ src/day_02/day_02.test.ts
@@ -1,26 +1,31 @@
-import { parseGame } from './day_02'
+import {
+  Game,
+  checkIfPossible,
+  getSumOfPossibleGameIds,
+  parseGame,
+} from './day_02'
 
 describe('parseGame()', () => {
   const testInputWithAnswers = [
     {
       input: 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
       expected: {
-        game: 1,
+        id: 1,
         sets: [
-          { blue: 3, red: 4 },
+          { blue: 3, red: 4, green: 0 },
           { blue: 6, green: 2, red: 1 },
-          { green: 2 },
+          { green: 2, blue: 0, red: 0 },
         ],
       },
     },
     {
       input: 'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
       expected: {
-        game: 2,
+        id: 2,
         sets: [
-          { blue: 1, green: 2 },
+          { blue: 1, green: 2, red: 0 },
           { green: 3, blue: 4, red: 1 },
-          { green: 1, blue: 1 },
+          { green: 1, blue: 1, red: 0 },
         ],
       },
     },
@@ -28,11 +33,11 @@ describe('parseGame()', () => {
       input:
         'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
       expected: {
-        game: 3,
+        id: 3,
         sets: [
           { green: 8, blue: 6, red: 20 },
           { blue: 5, red: 4, green: 13 },
-          { green: 5, red: 1 },
+          { green: 5, red: 1, blue: 0 },
         ],
       },
     },
@@ -40,10 +45,10 @@ describe('parseGame()', () => {
       input:
         'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
       expected: {
-        game: 4,
+        id: 4,
         sets: [
           { green: 1, red: 3, blue: 6 },
-          { green: 3, red: 6 },
+          { green: 3, red: 6, blue: 0 },
           { green: 3, blue: 15, red: 14 },
         ],
       },
@@ -51,7 +56,7 @@ describe('parseGame()', () => {
     {
       input: 'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green',
       expected: {
-        game: 5,
+        id: 5,
         sets: [
           { red: 6, blue: 1, green: 3 },
           { blue: 2, red: 1, green: 2 },
@@ -67,9 +72,125 @@ describe('parseGame()', () => {
     ${testInputWithAnswers[2].input} | ${testInputWithAnswers[2].expected}
     ${testInputWithAnswers[3].input} | ${testInputWithAnswers[3].expected}
     ${testInputWithAnswers[4].input} | ${testInputWithAnswers[4].expected}
-  `('given an input of $input, returns $expected', ({ input, expected }) => {
+  `('parses the input string into a Game format', ({ input, expected }) => {
     const result = parseGame(input)
 
     expect(result).toStrictEqual(expected)
   })
+})
+
+describe('checkIfPossible', () => {
+  const bag = {
+    blue: 14,
+    green: 13,
+    red: 12,
+  }
+
+  const testInputWithAnswers = [
+    {
+      input: [{ blue: 3, red: 4 }, { blue: 6, green: 2, red: 1 }, { green: 2 }],
+      expected: true,
+    },
+    {
+      input: [
+        { blue: 1, green: 2 },
+        { green: 3, blue: 4, red: 1 },
+        { green: 1, blue: 1 },
+      ],
+      expected: true,
+    },
+    {
+      input: [
+        { green: 8, blue: 6, red: 20 },
+        { blue: 5, red: 4, green: 13 },
+        { green: 5, red: 1 },
+      ],
+      expected: false,
+    },
+    {
+      input: [
+        { green: 1, red: 3, blue: 6 },
+        { green: 3, red: 6 },
+        { green: 3, blue: 15, red: 14 },
+      ],
+      expected: false,
+    },
+    {
+      input: [
+        { red: 6, blue: 1, green: 3 },
+        { blue: 2, red: 1, green: 2 },
+      ],
+      expected: true,
+    },
+  ]
+
+  test.each`
+    input                            | expected
+    ${testInputWithAnswers[0].input} | ${testInputWithAnswers[0].expected}
+    ${testInputWithAnswers[1].input} | ${testInputWithAnswers[1].expected}
+    ${testInputWithAnswers[2].input} | ${testInputWithAnswers[2].expected}
+    ${testInputWithAnswers[3].input} | ${testInputWithAnswers[3].expected}
+    ${testInputWithAnswers[4].input} | ${testInputWithAnswers[4].expected}
+  `(
+    'returns whether the guessed bag contents are possible given the observed results',
+    ({ input, expected }) => {
+      const result = checkIfPossible(input, bag)
+
+      expect(result).toBe(expected)
+    },
+  )
+})
+
+describe('getSumOfPossibleGameIds', () => {
+  const expected = 8
+
+  const input: Game[] = [
+    {
+      id: 1,
+      sets: [
+        { blue: 3, red: 4, green: 0 },
+        { blue: 6, green: 2, red: 1 },
+        { green: 2, red: 0, blue: 0 },
+      ],
+    },
+    {
+      id: 2,
+      sets: [
+        { blue: 1, green: 2, red: 0 },
+        { green: 3, blue: 4, red: 1 },
+        { green: 1, blue: 1, red: 0 },
+      ],
+    },
+    {
+      id: 3,
+      sets: [
+        { green: 8, blue: 6, red: 20 },
+        { blue: 5, red: 4, green: 13 },
+        { green: 5, red: 1, blue: 0 },
+      ],
+    },
+    {
+      id: 4,
+      sets: [
+        { green: 1, red: 3, blue: 6 },
+        { green: 3, red: 6, blue: 0 },
+        { green: 3, blue: 15, red: 14 },
+      ],
+    },
+    {
+      id: 5,
+      sets: [
+        { red: 6, blue: 1, green: 3 },
+        { blue: 2, red: 1, green: 2 },
+      ],
+    },
+  ]
+
+  const bag = {
+    blue: 14,
+    green: 13,
+    red: 12,
+  }
+
+  expect(getSumOfPossibleGameIds(input, bag)).toBe(expected)
 })
--- src/day_02/day_02.ts
+++ src/day_02/day_02.ts
@@ -4,15 +4,16 @@ type Set = {
   [key in Color]: number
 }
 
-interface Game {
-  game: number
+export interface Game {
+  id: number
   sets: Set[]
 }
 
 export const parseGame = (input: string): Game => {
+  const colors: Color[] = ['blue', 'green', 'red']
   const [rawGame, rawSets] = input.split(':')
 
-  const game = Number(rawGame.split(' ')[1])
+  const id = Number(rawGame.split(' ')[1])
 
   const sets = rawSets
     .split(';')
@@ -27,9 +28,70 @@ export const parseGame = (input: string): Game => {
         return acc
       }, {} as Set),
     )
+    .map(set => {
+      const setKeys = Object.keys(set)
+
+      colors.forEach(color => {
+        if (!setKeys.includes(color)) {
+          set[color] = 0
+        }
+      })
+
+      return set
+    })
 
   return {
-    game,
+    id,
     sets,
   }
+}
+
+const getMaxValues = (sets: Set[]) => {
+  return sets.reduce((acc, curr) => {
+    const keys = Object.keys(curr) as Color[]
+
+    keys.map(key => {
+      if (!acc[key]) {
+        acc[key] = curr[key]
+      } else {
+        acc[key] = Math.max(acc[key], curr[key])
+      }
+    })
+
+    return acc
+  })
+}
+
+export const checkIfPossible = (sets: Set[], bag: Set): boolean => {
+  const maxValues = getMaxValues(sets)
+
+  const possibleColors = (Object.keys(bag) as Color[]).map(color => {
+    if (maxValues[color] <= bag[color]) {
+      return true
+    } else {
+      return false
+    }
+  })
+
+  return possibleColors.every(value => value === true)
+}
+
+export const getSumOfPossibleGameIds = (games: Game[], bag: Set): number => {
+  const gamesWithPossibility = games.map(game => {
+    const { id, sets } = game
+    const possibility = checkIfPossible(sets, bag)
+    return {
+      id,
+      possibile: possibility,
+    }
+  })
+
+  const possibleGames = gamesWithPossibility.filter( game => game.possibile )
+
+  const sum = possibleGames.reduce((acc, curr) => {
+    acc += curr.id
+    return acc
+  }, 0)
+
+  return sum
 }
